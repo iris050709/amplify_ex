fcf1f358ccab8ad9510df3d275f5a16a1b096dbf
iris050709 <ic8281932@gmail.com>
2025-02-22 11:05:39 -0600

    URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch(" http://127.0.0.1:5000/users")
+    fetch(" http://3.135.213.9/users")
       .then((response) => response.json())
       .then((data) => setUsers(data))
       .catch((error) => console.error("Error al obtener usuarios:", error));
