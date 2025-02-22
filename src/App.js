0285f129f809bb7ee66e5035e75b7cad24ea6b60
iris050709 <ic8281932@gmail.com>
2025-02-22 10:59:30 -0600

    EDIT

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(" http://127.0.0.1:5000/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.error("Error al obtener usuarios:", error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Lista de Usuarios</h1>
+      <ul>
+        {users.map((user, index) => (
+          <li key={index}>
+            {user.nombre} - {user.email}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
