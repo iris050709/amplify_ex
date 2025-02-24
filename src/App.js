9c1dd839dfe06832f104a2f9ff9ee19d3799556d
iris050709 <ic8281932@gmail.com>
2025-02-23 21:54:15 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,21 +2,35 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
-    fetch(" http://127.0.0.1:5000/users")
+    fetch("https://3.135.213.9/users")
       .then((response) => response.json())
-      .then((data) => setUsers(data))
+      .then((data) => {
+        console.log("Datos recibidos:", data);
+        setUsers(data);
+      })
       .catch((error) => console.error("Error al obtener usuarios:", error));
   }, []);
+
+  const filteredUsers = users.filter((user) =>
+    user.nombre.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div>
       <h1>Lista de Usuarios</h1>
+      <input
+        type="text"
+        placeholder="Buscar por nombre"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
       <ul>
-        {users.map((user, index) => (
+        {filteredUsers.map((user, index) => (
           <li key={index}>
-            {user.nombre} - {user.email}
+            {user.nombre} - {user.email} - {user.last_name}
           </li>
         ))}
       </ul>
