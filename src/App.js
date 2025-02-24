5158355415e58015a06fc4ba19fe2b1070478c72
iris050709 <ic8281932@gmail.com>
2025-02-23 21:51:28 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [users, setUsers] = useState([]);
-
   useEffect(() => {
-    fetch(" http://127.0.0.1:5000/users")
+    fetch("https://3.135.213.9/users")
       .then((response) => response.json())
-      .then((data) => setUsers(data))
+      .then((data) => {
+        console.log("Datos recibidos:", data); // Debug para ver la respuesta
+        setUsers(data);
+      })
       .catch((error) => console.error("Error al obtener usuarios:", error));
   }, []);
 
@@ -16,7 +18,7 @@ function App() {
       <ul>
         {users.map((user, index) => (
           <li key={index}>
-            {user.nombre} - {user.email}
+            {user.nombre} - {user.email} - {user.last_name}
           </li>
         ))}
       </ul>
