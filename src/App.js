a9fcf7d10ed7cd51cfad769ce91db69380903cba
iris050709 <ic8281932@gmail.com>
2025-03-07 22:04:14 -0600

    RUTA

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [search, setSearch] = useState("");
 
   useEffect(() => {
-    fetch("https://18.219.121.253/users")
+    fetch("https://3.133.138.221/users")
       .then((response) => response.json())
       .then((data) => {
         console.log("Datos recibidos:", data);
